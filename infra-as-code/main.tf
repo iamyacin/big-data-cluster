a956db0e32a5dacf74ce89cb265427414b18c977
Yacin <yacin@pop-os.localdomain>
2021-12-11 23:59:35 +0100

    add budget limit

--- infra-as-code/main.tf
+++ infra-as-code/main.tf
@@ -11,6 +11,15 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+resource "aws_budgets_budget" "budget-limit" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "5.0"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2020-12-12_00.01"
+}
+
 resource "aws_vpc" "my-principal-vpc" {
   cidr_block       = "172.16.0.0/16"
   instance_tenancy = "default"
