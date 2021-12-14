fe894789322a9a231ce28634d3ee20a38cac52bb
Yacin <yacin@pop-os.localdomain>
2021-12-14 02:49:29 +0100

    add security group settings

--- infra-as-code/vpc-settings.tf
+++ infra-as-code/vpc-settings.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "my-principal-vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block       = "172.16.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
     Name = "big-data-vpc"
@@ -64,6 +65,32 @@ resource "aws_subnet" "big-data-subnet" {
 }
 
 resource "aws_route_table_association" "rt-to-subnet" {
-  subnet_id      = aws_subnet.big-data-subnet.id
-  route_table_id = aws_route_table.my-rt.id
+    subnet_id = aws_subnet.big-data-subnet.id
+    route_table_id = aws_route_table.my-rt.id 
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.my-principal-vpc.id
+
+  ingress {
+    description      = "SSH from all"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "ssh"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
 }
