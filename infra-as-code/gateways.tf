5c5e0030474011c01328829ca7899145e2fbb52b
Yacin <yacin@pop-os.localdomain>
2021-12-14 02:18:49 +0100

    reorganize terraform files and correct some aws configurations

--- infra-as-code/gateways.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "my-internet-gw" {
-  vpc_id = aws_vpc.my-principal-vpc.id
-
-  tags = {
-    Name = "my-internet-gw"
-  }
-}
--- infra-as-code/vpc.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_vpc" "my-principal-vpc" {
-  cidr_block       = "172.16.0.0/16"
-  instance_tenancy = "default"
-
-  tags = {
-    Name = "big-data-vpc"
-  }
-}
