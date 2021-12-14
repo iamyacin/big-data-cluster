bc673aa80aa29e416c6bb4b2a38bca68ab4f1cbc
Yacin <yacin@pop-os.localdomain>
2021-12-14 04:22:40 +0100

    go back to one main.tf file

--- infra-as-code/budget-settings.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_budgets_budget" "budget-limit" {
-  name              = "monthly-budget"
-  budget_type       = "COST"
-  limit_amount      = "5.0"
-  limit_unit        = "USD"
-  time_unit         = "MONTHLY"
-  time_period_start = "2020-12-14_02:30"
-}
--- infra-as-code/main.tf
+++ infra-as-code/main.tf
@@ -1,4 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.51.0"
+    }
+  }
+}
 
+provider "aws" {
+  region                  = "eu-west-3"
+  shared_credentials_file = "/home/yacin/.aws/credentials"
+}
+
+resource "aws_subnet" "big-data-subnet" {
+  vpc_id                  = aws_vpc.main-vpc.id
+  cidr_block              = "172.16.100.0/24"
+
+  tags = {
+    Name = "big-data-subnet"
+    Terraform   = "true"
+  }
+}
 
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -13,7 +35,7 @@ module "ec2_instance" {
   key_name                    = "mykey"
   associate_public_ip_address = "true"
   monitoring                  = "true"
-  vpc_security_group_ids      = aws_security_group.allow-ssh.id
+  vpc_security_group_ids      = ["sg-04e4d6d2b15206549"]
   subnet_id                   = aws_subnet.big-data-subnet.id
 
   tags = {
